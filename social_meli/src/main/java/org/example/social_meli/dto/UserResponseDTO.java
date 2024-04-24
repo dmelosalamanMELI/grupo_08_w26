90b041330a04d83093aee5d51532bd0fc573dc32
Daniel Augusto Melo Salamanca <danielaugusto.melosalamanca@mercadolibre.com.co>
2024-04-24 10:59:26 -0500

    Actualizamos anotaciones, se agrega la dependencia y agrega lista de clientes y sellers al repositorio de cliente

--- social_meli/src/main/java/org/example/social_meli/dto/UserResponseDTO.java
+++ social_meli/src/main/java/org/example/social_meli/dto/UserResponseDTO.java
@@ -1,9 +1,14 @@
 package org.example.social_meli.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.social_meli.model.User;
 
 import java.util.*;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDTO {
     private Integer user_id;
     private String user_name;
