4d9331a8a343c059dde0bf93ad604d368f79c519
dbellolopez <diana.bellolopez@mercadolibre.com.co>
2024-05-07 11:12:20 -0500

    T-0001 hecho, para service y para repositorio

---
+++ social_meli/src/test/java/org/example/social_meli/repository/impl/UserRepositoryTest.java
@@ -0,0 +1,68 @@
+package org.example.social_meli.repository.impl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.example.social_meli.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserRepositoryTest {
+    @Mock
+    private List<User> userList;
+
+    @InjectMocks
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setup(){
+        Mockito.reset(userList);
+        User user1 = new User(2,"User1",true);
+        User user2 = new User(1,"User2",false);
+        List <User> users =new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        when(userList.stream()).thenReturn(users.stream());
+    }
+
+    @Test
+    @DisplayName("Verificar método findByID, usuario existe.")
+    void findById_UserExist() {
+        Integer userId = 1;
+        User found = userRepository.findById(userId);
+        Assertions.assertEquals(userId, found.getUser_id(), "El ID del usuario encontrado debe coincidir con el buscado.");
+        Assertions.assertNotNull(found, "El usuario encontrado no debe ser nulo.");
+    }
+    @Test
+    @DisplayName("Verificar método findByID, usuario no existe.")
+    void findById_UserNoExist() {
+        Integer userId = 5;
+        User found = userRepository.findById(userId);
+        Assertions.assertNull(found, "Debe devolver null si el usuario no existe.");
+    }
+    @Test
+    @DisplayName("Veficar método exixtByID, usuario existente.")
+    void existById_UserExits(){
+        Integer userId = 1;
+        Boolean exists = userRepository.existsById(userId);
+        assertTrue(exists, "El método debería retornar verdadero para un usuario existente");
+    }
+    @Test
+    @DisplayName("Veficar método exixtByID, usuario no existente.")
+    void existById_UserNoExits(){
+        Integer userId = 5;
+        Boolean exists = userRepository.existsById(userId);
+        assertFalse(exists, "El método debería retornar falso para un usuario existente");
+    }
+}
--- social_meli/src/test/java/org/example/social_meli/services/impl/UserServiceImplTest.java
+++ social_meli/src/test/java/org/example/social_meli/services/impl/UserServiceImplTest.java
@@ -1,17 +1,21 @@
 package org.example.social_meli.services.impl;
 
 import org.example.social_meli.exceptions.BadRequestException;
+import org.example.social_meli.model.User;
 import org.example.social_meli.repository.IUserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.MockitoAnnotations;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
     /*Crear Mock*/
     @Mock
@@ -19,13 +23,38 @@ class UserServiceImplTest {
     /*Inyectar Mocks*/
     @InjectMocks
     private UserServiceImpl userService;
+
     @BeforeEach
     public void setup(){
-        MockitoAnnotations.openMocks(this);
+    }
+    @Test
+    /*Verificar que el usuario a seguir exista. (US-0001).*/
+    @DisplayName("El usuario no es vendedor")
+    void usernotVendedor() {
+        /*A Instanciar objeto*/
+        Integer userIdValid=1;
+        Integer userIdToFollow=2;
+        User client = new User(userIdValid, "cliente",false);
+        User seller = new User(userIdToFollow, "vendedor",false);
+        when(userRepository.existsById(userIdValid)).thenReturn(true);
+        when(userRepository.existsById(userIdToFollow)).thenReturn(true);
+        when(userRepository.findById(userIdValid)).thenReturn(client);
+        when(userRepository.findById(userIdToFollow)).thenReturn(seller);
+        /*Arrange*/
+        assertThrows(
+                BadRequestException.class,
+                () -> userService.followUser(userIdValid, userIdToFollow),
+                "No puede seguirlo, el usuario no es vendedor."
+        );
+        /*Assertions*/
+        verify(userRepository, times(1)).existsById(userIdValid);
+        verify(userRepository, times(1)).existsById(userIdToFollow);
+        verify(userRepository, times(1)).findById(userIdValid);
+        verify(userRepository, times(1)).findById(userIdToFollow);
     }
     @Test
     @DisplayName("Verificar que el usuario a seguir exista. (US-0001).")
-    void followUser() {
+    void existUser() {
         /*A Instanciar objeto*/
         Integer userIdValid=1;
         Integer userIdNotValid=2;
