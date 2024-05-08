dfe8fd947ca037e9aeecb14593df85e1a6a9fdcb
dbellolopez <diana.bellolopez@mercadolibre.com.co>
2024-05-08 18:21:32 -0500

    Tests individuales

--- social_meli/src/test/java/org/example/social_meli/controller/UserControllerTest.java
+++ social_meli/src/test/java/org/example/social_meli/controller/UserControllerTest.java
@@ -1,43 +1,57 @@
 package org.example.social_meli.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.example.social_meli.dto.UserResponseDTO;
-import org.junit.jupiter.api.DisplayName;
+import org.example.social_meli.services.IUserService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
-
+    @MockBean
+    private IUserService userService;
     @Test
-    @DisplayName("Deberia dejar de seguir a un usuario")
-    void postUnfollowUser() throws Exception {
-        Integer userId = 1;
-        Integer userIdToUnfollow = 2;
+    @DisplayName("Integration test follower User")
+    void testFollowerUser() throws Exception {
+        Integer followerId=1;
+        Integer followedId=2;
 
-        String url = String.format("/users/%d/unfollow/%d", userId, userIdToUnfollow);
-
-        mockMvc.perform(MockMvcRequestBuilders.post(url)
-                .contentType("application/json"))
-                .andDo(print())
-                .andExpect(content().string("{\"user_id\":1,\"user_name\":\"wcalderwood0\",\"follower\":[]}"))
-                .andExpect(status().isOk());
+        doNothing().when(userService).followUser(followerId,followedId);
+        String url = String.format("/users/%d/follow/%d", followerId, followedId);
+        mockMvc.perform(MockMvcRequestBuilders.post(url).contentType("application/json")).andExpect(status().isOk());
     }
-}
+    @Test
+    @DisplayName("Get Followers Test")
+    void testGetFollowers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/1/followers/list")
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(result -> {
+                            if (result.getResponse().getContentAsString().isEmpty()) {
+                                System.out.println("No content returned.");
+                            } else {
+                                assertThat(result.getResponse().getContentAsString()).isNotEmpty();
+                            }
+                        }
+                );
+        }
+
+    }
--- social_meli/src/test/java/org/example/social_meli/services/impl/UserServiceImplTest.java
+++ social_meli/src/test/java/org/example/social_meli/services/impl/UserServiceImplTest.java
@@ -17,11 +17,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
-
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -428,6 +426,5 @@ class UserServiceImplTest {
         //Assert
         assertThat(result,samePropertyValuesAs(expectedResult));
     }
-
 
 }
